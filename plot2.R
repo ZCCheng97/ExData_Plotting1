d7ec825f457ddb1c789f43ef586226f405cddf21
ZCCheng97 <zccheng97@gmail.com>
2020-06-18 13:01:34 +0800

    Edited codes for plot1.R, plot2.R and plot3.R to generate the png of the plot.
    Generated png of the plots plot1, plot2 and plot3.

--- plot2.R
+++ plot2.R
@@ -3,4 +3,7 @@ newtab <- table[table$Date %in% c("1/2/2007","2/2/2007"),]
 newtab$datetime <- paste(newtab$Date,newtab$Time)
 newtab$Date <- strptime(newtab$datetime,format = "%d/%m/%Y %H:%M:%S")
 exam <- na.omit(as.numeric(newtab$Global_active_power))
+
+png("plot2.png")
 plot(x= newtab$Date, y = exam, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+dev.off()
