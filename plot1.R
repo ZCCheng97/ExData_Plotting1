4518b39fd1ffe0c486313bfcd7b9a0fb412d85f3
ZCCheng97 <zccheng97@gmail.com>
2020-06-18 11:31:33 +0800

    Created code for plot1.R Created code for plot2.R

---
+++ plot1.R
@@ -0,0 +1,5 @@
+table <- read.table("household_power_consumption.txt", sep = ";", header = T, stringsAsFactors = F)
+newtab <- table[table$Date %in% c("1/2/2007","2/2/2007"),]
+exam <- na.omit(as.numeric(newtab$Global_active_power))
+hist(exam,main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "Red", breaks = seq(0, 12, by = 0.5),xlim = c(0,6), ylim = c(0,1200), yaxt = "n")
+axis(2, at=seq(0, 1200, by = 200), labels=seq(0, 1200, by = 200))
---
+++ plot2.R
@@ -0,0 +1,6 @@
+table <- read.table("household_power_consumption.txt", sep = ";", header = T, stringsAsFactors = F)
+newtab <- table[table$Date %in% c("1/2/2007","2/2/2007"),]
+newtab$datetime <- paste(newtab$Date,newtab$Time)
+newtab$Date <- strptime(newtab$datetime,format = "%d/%m/%Y %H:%M:%S")
+exam <- na.omit(as.numeric(newtab$Global_active_power))
+plot(x= newtab$Date, y = exam, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
